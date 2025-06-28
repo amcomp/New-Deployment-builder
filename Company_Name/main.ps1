30b633725601ccd0b7c95b920251d6105735e8e1
Mathew <mathew.titus@outlook.com>
2025-06-28 17:38:38 -0400

    Added check to ensure .\config exists before continuing, creates folder if not exist

--- Company_Name/main.ps1
+++ Company_Name/main.ps1
@@ -8,6 +8,13 @@ foreach ($module in $imports) {
     import-module .\functions\$module -Force
 }
 
+$configPath = Test-Path ".\config"
+if (-not $configPath) {
+    Write-Host "Error: Configuration folder not found at .\config"
+    New-Item -ItemType Directory -Path .\config
+}
+
+
 switch ($Action) {
     "Extract" {
         Get-InstalledApps
