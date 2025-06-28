dd63e3a1ce38087b2e8bae80faba95ea33af3363
Mathew <mathew.titus@outlook.com>
2025-06-28 16:52:01 -0400

    imports to dynamically load all modules from the functions directory

--- Company_Name/main.ps1
+++ Company_Name/main.ps1
@@ -2,10 +2,11 @@ param (
     [string]$Action
 )
 
-# Import your custom module
-Import-Module .\functions\GetInstalledApps.psm1 -Force
-Import-Module .\functions\Generate-List.psm1 -Force
-Import-Module .\functions\Install-Apps.psm1 -Force
+# Loops the .\functions directory and imports all modules found there.
+$imports = (Get-ChildItem -Path .\functions).Name
+foreach ($module in $imports) {
+    import-module .\functions\$module -Force
+}
 
 switch ($Action) {
     "Extract" {
